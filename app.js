077d9283f2be989b1ce94800724417f30cb074e1
Jyoti Santani <jyotisantani@Jyotis-MacBook-Air.local>
2024-05-04 21:19:58 +0530

    final-commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
